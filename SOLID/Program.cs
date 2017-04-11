a2bed3bf3873b24e9f98a3278e2ff63e5e175ba1
Naveen Kanikaram <nkanikaram@gmail.com>
2017-04-11 12:23:46 -0500

    Check in more changes.

--- SOLID/Program.cs
+++ SOLID/Program.cs
@@ -1,6 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Net.Sockets;
+using System.Security.Policy;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -10,6 +13,117 @@ namespace SOLID
     {
         static void Main(string[] args)
         {
+            List<Customer> obj = new List<Customer> {new GoldCustomer(), new SilverCustomer()};
+            foreach (Customer customer in obj)
+            {
+                customer.AddCustomer();    
+            }
+
+
+            ICustomer custOld = new Customer();
+            IRead custRead = new Customer();
+
+            custRead.CalculateDiscount();
+
+        }
+
+        public interface IErrorHandler
+        {
+            void HandleError(string err);
+        }
+
+        public class FileErrorHandler : IErrorHandler
+        {
+            public void HandleError(string err)
+            {
+                File.WriteAllText("", err);
+            }
+        }
+
+        public class EventVwrErr : IErrorHandler
+        {
+            public void HandleError(string err)
+            {
+                //throw new NotImplementedException();
+            }
+        }
+
+        public class  SqlErr:IErrorHandler
+        {
+            public void HandleError(string err)
+            {
+                //throw new NotImplementedException();
+            }
+        }
+
+
+
+        public class Customer : ICustomer
+        {
+            public virtual double CalculateDiscount()
+            {
+                return 0;
+            }
+
+            public virtual void AddCustomer()
+            {
+                try
+                {
+
+                }
+                catch(Exception ex)
+                {
+                    ErrorLogger el = new ErrorLogger();
+                    el.LogException(ex.ToString());
+                }
+            }
+        }
+
+        class ErrorLogger
+        {
+            public void LogException(string errString)
+            {
+                File.WriteAllText(@"C:/Test.txt", errString);
+
+            }
+        }
+        public class GoldCustomer: Customer
+        {
+            public override double CalculateDiscount()
+            {
+                return base.CalculateDiscount() + 10;
+            }
+        }
+
+        public class  SilverCustomer: Customer
+        {
+            
+            public override double CalculateDiscount()
+            {
+                return base.CalculateDiscount() + 5;
+            }
+        }
+
+        public class EnquiryCustomer : IEnquiry
+        {
+            public double CalculateDiscount()
+            {
+                return 2;
+            }
+        }
+
+        public interface IEnquiry
+        {
+            double CalculateDiscount();
+        }
+
+        public interface ICustomer : IEnquiry
+        {
+            void AddCustomer();
+        }
+        public interface IRead:ICustomer
+        {
+            void Read();
         }
     }
 }
